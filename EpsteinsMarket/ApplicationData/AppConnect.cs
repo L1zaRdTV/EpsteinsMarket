65dcce49ca9073b6fc3350c45b24b877bb5dc6f9
𝗟𝟏𝘇𝗮𝗥𝗱? <daniilgridasov31@gmail.com>
2026-06-16 20:28:31 +0300

    Refactor warehouse app with offline database fallback

--- EpsteinsMarket/ApplicationData/AppConnect.cs
+++
@@ -1,9 +0,0 @@
-
-namespace EpsteinMarket.ApplicationData
-{
-    internal class AppConnect
-    {
-        public static WarehouseDBEntitiesContext model01;
-        public static Users CurrentUser;
-    }
-}
